c00c4e9790096132d97a4643f539d3e6f797d133
Andre Van Der Merwe <andre@andrevdm.com>
2018-07-09 18:46:03 +0200

    hackage docs upload script
    See bottom of https://hackage.haskell.org/upload

---
+++ mkDocs.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+dir=$(mktemp -d dist-docs.XXXXXX)
+trap 'rm -r "$dir"' EXIT
+
+cabal new-haddock --builddir="$dir" --haddock-option=--hyperlinked-source --haddock-for-hackage
+
+# Starting with cabal 2.0, `--publish` is needed for uploading to non-candidate releases
+cabal upload -d $dir/*-docs.tar.gz 
+
